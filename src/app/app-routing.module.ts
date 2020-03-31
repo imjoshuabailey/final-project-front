a6231062d9345e8703c7eaa2ed68a2bfbcaea801
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-03-30 21:47:16 -0700

    moved genre display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component'
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component'
+import { ProfileComponent } from './profile/profile.component'
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'main', component: MainComponent},
+  { path: 'profile', component: ProfileComponent},
   { path: '**', redirectTo: 'main', pathMatch: 'full' },
 ];
 
